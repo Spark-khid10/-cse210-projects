b29da5f224939bbfcbce5dee2be78539a798c101
Ephraim Otikor <ephraimotikor97@gmail.com>
2026-03-21 15:37:49 +0100

    Update .gitignore and Exercise2 program

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your score? ");
+        string score = Console.ReadLine();
+        int number = int.Parse(score);
+        string letter;
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "c";
+        }
+        else
+        {
+            letter = "F";
+        }   
+
+         int lastDigit = number % 10;
+         string sign = "";
+
+         if (lastDigit > 7)
+        {
+             sign = "+";
+        }
+         else if (lastDigit < 3)
+        {
+             sign = "-";
+        }
+        if (letter == "A" && sign == "+" )
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter} {sign}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, You passed this course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep Your head up, You're getting there. Try again.");
+        }
+
     }
 }
